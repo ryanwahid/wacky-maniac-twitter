1448e45edc1ed05020354b15968fd2dbbc762d33
ryanwahid <ryanwahid@gmail.com>
2018-06-04 17:50:29 -0500

    just test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,4 +5,17 @@ RSpec.describe User, type: :model do
 		user = User.new(email: 'jojo@gmail.com', name: 'mojo jojo', username: 'mojojojo', bio:'crazy evil monkey', location: 'townsville') 
 		expect(user).to be_valid
 	end
+
+	it "is invalid without a username" do
+		user = User.new(email: 'jojo@gmail.com', name: 'mojo jojo', username: nil, bio:'crazy evil monkey', location: 'townsville') 
+	end
+
+	it "is invalid without a email" do
+		user = User.new(email: nil, name: 'mojo jojo', username: nil, bio:'crazy evil monkey', location: 'townsville') 
+	end
+
+	it "is invalid without a password" do
+		user = User.new(email: nil, password: nil, name: 'mojo jojo', username: 'mojojo', bio:'crazy evil monkey', location: 'townsville') 
+	end
+
 end

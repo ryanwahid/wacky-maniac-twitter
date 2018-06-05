9b21c400ffffa80ebb3b41be364553153190b639
ryanwahid <ryanwahid@gmail.com>
2018-06-05 14:59:13 -0500

    added sign out btn

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :tweets
   devise_for :users
-  get 'welcome/index'
+  
+  get '/' => 'tweets#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
